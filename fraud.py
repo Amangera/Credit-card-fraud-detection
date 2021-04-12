05e1e125461dc442da89d3a0398808d10063a01e
anurag pachauri <46953966+anuragpachauri@users.noreply.github.com>
2021-04-12 14:21:41 +0530

    Update fraud.py

--- fraud.py
+++ fraud.py
@@ -46,3 +46,74 @@ print(cl('--------------------------------------------', attrs = ['bold']))
 print(cl('FRAUD CASE AMOUNT STATS', attrs = ['bold']))
 print(fraud_cases.Amount.describe())
 print(cl('--------------------------------------------', attrs = ['bold']))
+# DATA SPLIT
+
+X = df.drop('Class', axis = 1).values
+y = df['Class'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+print(cl('X_train samples : ', attrs = ['bold']), X_train[:1])
+print(cl('X_test samples : ', attrs = ['bold']), X_test[0:1])
+print(cl('y_train samples : ', attrs = ['bold']), y_train[0:10])
+print(cl('y_test samples : ', attrs = ['bold']), y_test[0:10])
+
+# MODELING
+# 3. Logistic Regression
+
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+lr_yhat = lr.predict(X_test)
+
+# EVALUATION
+
+# 1. Accuracy score
+print(cl('Accuracy score of the Logistic Regression model is {}'.format(accuracy_score(y_test, lr_yhat)), attrs = ['bold'], color = 'red'))
+print(cl('------------------------------------------------------------------------', attrs = ['bold']))
+
+# 2. F1 score
+print(cl('F1 score of the Logistic Regression model is {}'.format(f1_score(y_test, lr_yhat)), attrs = ['bold'], color = 'red'))
+print(cl('------------------------------------------------------------------------', attrs = ['bold']))
+
+# 3. Confusion Matrix
+
+# defining the plot function
+
+def plot_confusion_matrix(cm, classes, title, normalize = False, cmap = plt.cm.Blues):
+    title = 'Confusion Matrix of {}'.format(title)
+    if normalize:
+        cm = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation = 45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment = 'center',
+                 color = 'white' if cm[i, j] > thresh else 'black')
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+    # Compute confusion matrix
+    matrix = confusion_matrix(y_test, lr_yhat, labels = [0, 1]) # Logistic Regression
+    
+    # Plot the confusion matrix
+
+plt.rcParams['figure.figsize'] = (6, 6)
+    
+    
+    # jk Logistic regression
+
+lr_cm_plot = plot_confusion_matrix(matrix, 
+                                classes = ['Non-Default(0)','Default(1)'], 
+                                normalize = False, title = 'Logistic Regression')
+plt.savefig('lr_cm_plot.png')
+plt.show()
